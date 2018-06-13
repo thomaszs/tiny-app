333dac58f7180dabe52a7db18ce29c881acbd7a3
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-13 20:09:37 +0000

    Day 2 - added delete and edit function with post request

--- express_server.js
+++ express_server.js
@@ -2,46 +2,69 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+// Used this function to generate random 6 digit alphanumeric short url
 function generateRandomString() {
-  return Math.random().toString(36).substring(2,8)
+  return Math.random().toString(36).substring(2, 8) // Used substring instead of slice to get strings
 }
 
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+// Defined database
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+// POST request for /urls
 app.post("/urls", (req, res) => {
-  let longURL = (req.body);  // debug statement to see POST parameters
+  let longURL = (req.body); //Took input from the form 
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL["longURL"];
-  console.log(urlDatabase);
-  res.redirect("/urls/" + shortURL);         // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL] = longURL["longURL"]; //Used short URL as a key in object, used long URL as value
+  res.redirect("/urls/" + shortURL); // Redirected to /urls/:shortURL
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+// POST delete on /urls/:id request by using javascript delete method
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]; 
+  res.redirect("/urls/");
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
-});
-
 app.get("/urls/:id", (req, res) => {
   let longURL = urlDatabase[req.params.id];
-  let templateVars = { shortURL: req.params.id, longURL: longURL };
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: longURL
+  };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL
+  res.redirect("/urls/");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
+
+// Used a separate /u/ directory to test out redirect
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]; // Used shortURL as key to get long URL value
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
