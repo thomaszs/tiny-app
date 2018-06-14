daef08d709c116756e1193559cdcacad8acae73f
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-14 03:08:43 +0000

    Added registration page/function and handled error

--- express_server.js
+++ express_server.js
@@ -22,10 +22,26 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -61,6 +77,29 @@ app.get("/urls/:id", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL]; // Used shortURL as key to get long URL value
   res.redirect(longURL);
+});
+
+// POST for register
+app.post("/register", (req, res) => {
+  let id = generateRandomString();
+  let email = req.body.email;
+  let password = req.body.password;
+  users[id] = {id:id, email:email, password:password};
+  res.cookie("user_id", id);  
+
+  //Check for empty email or password and send 400 status code
+  if (!email || !password) {
+    res.statusCode = 400;
+    res.send("Email or password can't be empty")
+  }
+
+  //Check for exising email address and send 400 status code
+  for (var duplicate in users) {
+    if (users[duplicate]["email"] === email)
+        res.statusCode = 400
+        res.send("User already exist")      
+  } 
+  res.redirect("/urls/"); 
 });
 
 // Use POST request to set user cookie
